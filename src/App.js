79c85f9eb60c34fd7b5022003179ad4276282bdb
spukkala <spukkala@woolworths.com.au>
2020-08-11 12:50:34 +1000

    Basic header is setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      Instagram Clone
+      {/** Header */}
+      <div className="app__header">
+        <img className="app__headerImage"
+         src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
+         alt="Instagram" />
+      </div>
+      
+
+      {/** Posts */}
+      {/** Posts */}
     </div>
   );
 }
