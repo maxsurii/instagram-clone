fdeadbb2dafb4d745c9e1f7df33811bbd06c9929
spukkala <spukkala@woolworths.com.au>
2020-08-11 21:54:45 +1000

    Rerender the page with unique keyid using document id

--- src/App.js
+++ src/App.js
@@ -6,25 +6,15 @@ import { db } from './firebase';
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {
-      userName: "Suri",
-      caption: "Wow it works",
-      imageUrl:
-        "https://scontent.fsyd3-1.fna.fbcdn.net/v/t1.0-9/117116874_1018723381892569_5721145167485563021_n.jpg?_nc_cat=105&_nc_sid=8024bb&_nc_ohc=8lVMJXYOVcsAX9KYN4a&_nc_ht=scontent.fsyd3-1.fna&oh=b6cd0979e7c8e259ee99b1a698bbb064&oe=5F58678D",
-    },
-    {
-      userName: "Aeroplane",
-      caption: "NBC OImage",
-      imageUrl:
-        "https://nbc.yepbooking.com.au/userfiles/header/logo-mobile.png",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
       // Everytime new post is added this will triggered
-      setPosts(snapshot.docs.map(doc => doc.data()));
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
     })
   }, [posts])
 
@@ -39,12 +29,13 @@ function App() {
         />
       </div>
       {/** Posts */}
-      {posts.map(post => (
-         <Post
-         userName={post.userName}
-         caption={post.caption}
-         imageUrl={post.imageUrl}
-       />
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          userName={post.userName}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
       ))}
     </div>
   );
