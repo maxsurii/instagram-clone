262ab68682672827a60e9176d60ad3398b92e7b9
spukkala <spukkala@woolworths.com.au>
2020-08-12 18:02:40 +1000

    ImageUpload code structure

---
+++ src/ImageUpload.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ImageUpload.css'
+
+function ImageUpload() {
+    return (
+        <div className="imageUpload">
+            {/** File Picker */}
+            {/** Post Button */}
+        </div>
+    )
+}
+
+export default ImageUpload
